8bd091534f42b67752aa23ddda78469a72001013
tanjoin <tanjolno@gmail.com>
2012-12-03 21:08:49 +0900

    progress表示を追加

--- breaktime.h
+++ breaktime.h
@@ -1,14 +1,34 @@
 #ifndef _BREAKTIME_H_
 #define _BREAKTIME_H_
 
+#include <iostream>
+#include <string>
+#include <vector>
 #include <ctime>
 
-// ŽžŠÔ‘Ò‚¿
-void breaktime(double s){
-	clock_t t;
 
-	t = clock() + s;
-	while(t > clock());
-}
+class ConsoleProgressViewer {
+public:
+  // ŽžŠÔ‘Ò‚¿
+  static void breaktime(double s) {
+	  clock_t t;
+	  t = clock() + s;
+	  while(t > clock());
+  }
+
+  // •\Ž¦
+  static void progress(std::vector<std::string> message, double delay = 100.0) {
+
+    for (int num = 0; num < message.size(); num++) {
+      for (int length = 0; length < message[num].size(); length++) {
+        std::cout << message[num][length];
+        breaktime(delay);
+      }      
+    }
+    std::cout << std::endl;
+  }
+
+};
+
 
 #endif // _BREAKTIME_H_
