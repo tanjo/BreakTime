4ff1e1a0b5c510156645369485b7f5bc60abc9f2
tanjoin <tanjolno@gmail.com>
2012-12-03 21:27:53 +0900

    1行表示に対応(\nを使うな)

--- README.md
+++ README.md
@@ -4,6 +4,20 @@ BreakTime
 疑似スリープ  
 
     int main(void) {
-      breaktime(1000);
+      ConsoleProgressViewer::breaktime(1000);
+      
+      std::vector<std::string> message;
+      
+      message.push_back("問題です！");
+      message.push_back("これは一体何でしょう？");
+      message.push_back("1");
+      message.push_back("2");
+      message.push_back("3");
+      message.push_back("はい終了！");
+      message.push_back("答えは■■■でした！");
+      
+      ConsoleProgressViewer::progress(message, 10);
+      ConsoleProgressViewer::progressWithOneLine(message, 100);
+      
       return 0;
     }
--- breaktime.h
+++ breaktime.h
@@ -28,6 +28,25 @@ public:
     std::cout << std::endl;
   }
 
+  // one line show message (Win & Linux)
+  static void progressWithOneLine(std::vector<std::string> message, double delay = 100.0) {
+
+    for (int num = 0; num < message.size(); num++) {
+      for (int length = 0; length < message[num].size(); length++) {
+        std::cout << message[num][length];
+        breaktime(delay);
+      }
+      breaktime(delay);
+      std::cout << "\r";
+      for (int i = 0; i < message[num].size(); i++) {
+        std::cout << "@";
+      }
+      std::cout << "\r";
+    }
+    std::cout << std::endl;
+
+  }
+
 };
 
 
